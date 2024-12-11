1c01a1213eb6c5e267aa9a7ce3433e77907a2fae
unknown <goirkopf@gmail.com>
2024-12-11 18:31:21 +0300

    фикс анимации врачи

--- src/components/TeamSwiper.tsx
+++ src/components/TeamSwiper.tsx
@@ -54,31 +54,31 @@ const persons: IPerson[] = [
   { id: 6, photo: person6, name: "6", specialization: "6" },
   { id: 7, photo: person7, name: "7", specialization: "7" },
   { id: 8, photo: person8, name: "8", specialization: "8" },
-  { id: 9, photo: person9, name: "Доктор И", specialization: "Офтальмолог" },
-  { id: 10, photo: person10, name: "Доктор К", specialization: "ЛОР" },
-  { id: 11, photo: person11, name: "Доктор Л", specialization: "Кардиолог" },
-  { id: 12, photo: person12, name: "Доктор М", specialization: "Гинеколог" },
-  { id: 13, photo: person13, name: "Доктор Н", specialization: "Терапевт" },
-  { id: 14, photo: person14, name: "Доктор О", specialization: "Кардиолог" },
-  { id: 15, photo: person15, name: "Доктор П", specialization: "Уролог" },
-  { id: 16, photo: person16, name: "Доктор Р", specialization: "Пульмонолог" },
-  { id: 17, photo: person17, name: "Доктор С", specialization: "Хирург" },
-  { id: 18, photo: person18, name: "Доктор Т", specialization: "Терапевт" },
-  { id: 19, photo: person19, name: "Доктор У", specialization: "Эндокринолог" },
-  { id: 20, photo: person20, name: "Доктор Ф", specialization: "Невролог" },
-  { id: 21, photo: person21, name: "Доктор Х", specialization: "Педиатр" },
-  { id: 22, photo: person22, name: "Доктор Ц", specialization: "фывфыв" },
-  { id: 23, photo: person23, name: "Доктор Ч", specialization: "Офтальмолог" },
-  { id: 24, photo: person24, name: "Доктор Ш", specialization: "Стоматолог" },
-  { id: 25, photo: person25, name: "Доктор Щ", specialization: "Кардиолог" },
-  { id: 26, photo: person26, name: "Доктор Ы", specialization: "ЛОР" },
-  { id: 27, photo: person27, name: "Доктор Э", specialization: "Гинеколог" },
-  { id: 28, photo: person28, name: "Доктор Ю", specialization: "Терапевт" },
-  { id: 29, photo: person29, name: "Доктор Я", specialization: "Хирург" },
-  { id: 30, photo: person30, name: "Доктор Z", specialization: "Ортопед" },
+  // { id: 9, photo: person9, name: "Доктор И", specialization: "Офтальмолог" },
+  // { id: 10, photo: person10, name: "Доктор К", specialization: "ЛОР" },
+  // { id: 11, photo: person11, name: "Доктор Л", specialization: "Кардиолог" },
+  // { id: 12, photo: person12, name: "Доктор М", specialization: "Гинеколог" },
+  // { id: 13, photo: person13, name: "Доктор Н", specialization: "Терапевт" },
+  // { id: 14, photo: person14, name: "Доктор О", specialization: "Кардиолог" },
+  // { id: 15, photo: person15, name: "Доктор П", specialization: "Уролог" },
+  // { id: 16, photo: person16, name: "Доктор Р", specialization: "Пульмонолог" },
+  // { id: 17, photo: person17, name: "Доктор С", specialization: "Хирург" },
+  // { id: 18, photo: person18, name: "Доктор Т", specialization: "Терапевт" },
+  // { id: 19, photo: person19, name: "Доктор У", specialization: "Эндокринолог" },
+  // { id: 20, photo: person20, name: "Доктор Ф", specialization: "Невролог" },
+  // { id: 21, photo: person21, name: "Доктор Х", specialization: "Педиатр" },
+  // { id: 22, photo: person22, name: "Доктор Ц", specialization: "фывфыв" },
+  // { id: 23, photo: person23, name: "Доктор Ч", specialization: "Офтальмолог" },
+  // { id: 24, photo: person24, name: "Доктор Ш", specialization: "Стоматолог" },
+  // { id: 25, photo: person25, name: "Доктор Щ", specialization: "Кардиолог" },
+  // { id: 26, photo: person26, name: "Доктор Ы", specialization: "ЛОР" },
+  // { id: 27, photo: person27, name: "Доктор Э", specialization: "Гинеколог" },
+  // { id: 28, photo: person28, name: "Доктор Ю", specialization: "Терапевт" },
+  // { id: 29, photo: person29, name: "Доктор Я", specialization: "Хирург" },
+  // { id: 30, photo: person30, name: "Доктор Z", specialization: "Ортопед" },
 ];
 
-const ANIMATION_INTERVAL = 1500;
+const ANIMATION_INTERVAL =3000;
 
 export const TeamSwiper = () => {
   const breakpoint = useIsMobile();
@@ -92,49 +92,50 @@ export const TeamSwiper = () => {
   const [doctorInfo, setDoctorInfo] = useState<IPerson>();
   // const [isBlockVisible, setIsBlockVisible] = useState(false);
 
-
   useEffect(() => {
     const images = persons.map((person) => person.photo);
     preloadImages(images);
   }, []);
 
   useEffect(() => {
-
-
     const changeDoctor = () => {
       if (activeIndex !== null) {
         // setIsBlockVisible(false);
+        const updatedDoctors = [...visibleDoctors];
+        const remainingDoctors = persons.filter(
+          (doctor) => !visibleDoctors.some((v) => v.id === doctor.id),
+        );
+        const newDoctor =
+          remainingDoctors[Math.floor(Math.random() * remainingDoctors.length)];
+
+        updatedDoctors[activeIndex] = newDoctor;
+
+        const availableIndexes = visibleDoctors
+          .map((_, index) => index)
+          .filter((index) => index !== activeIndex);
+        const nextIndex =
+          availableIndexes[Math.floor(Math.random() * availableIndexes.length)];
+
+        const oldDoc = updatedDoctors[nextIndex];
+        setTimeout(() => {
+          setDoctorInfo(oldDoc);
+        }, 100); // на милисекунду видно инфу по старому врачу
 
         setTimeout(() => {
-
-
-          const remainingDoctors = persons.filter(
-            (doctor) => !visibleDoctors.some((v) => v.id === doctor.id),
-          );
-          const newDoctor =
-            remainingDoctors[Math.floor(Math.random() * remainingDoctors.length)];
-      
-          setTimeout(() => {
-            setDoctorInfo(newDoctor);
-          }, 500); // на милисекунду видно инфу по старому врачу
-
-          const updatedDoctors = [...visibleDoctors];
-          updatedDoctors[activeIndex] = newDoctor;
-          setVisibleDoctors(updatedDoctors);
-
-          const availableIndexes = visibleDoctors
-            .map((_, index) => index)
-            .filter((index) => index !== activeIndex);
-          const nextIndex =
-            availableIndexes[
-              Math.floor(Math.random() * availableIndexes.length)
-            ];
           setActiveIndex(nextIndex);
 
           // setIsBlockVisible(true);
-        }, 800);
+        }, 0);
+
+        setTimeout(() => {
+          setVisibleDoctors(updatedDoctors);
+
+          // setIsBlockVisible(true);
+        }, 0);
       } else {
-        setActiveIndex(Math.floor(Math.random() * visibleDoctors.length));
+        setActiveIndex(0);
+        setDoctorInfo(persons[0]);
+
         // setIsBlockVisible(true);
       }
     };
@@ -144,12 +145,14 @@ export const TeamSwiper = () => {
     return () => clearInterval(interval);
   }, [activeIndex, persons, visibleDoctors]);
 
+
+
+
   return (
     <div className="grid grid-cols-2 gap-[15px] desktop:grid-cols-6 desktop:gap-[20px]">
       {visibleDoctors.map((person, index) => (
         <div key={index} className="flex justify-center">
           <div className="relative h-full w-full cursor-pointer overflow-hidden rounded-[25px]">
-
             <DoctorInfo
               activeIndex={activeIndex}
               index={index}
@@ -169,38 +172,39 @@ export const TeamSwiper = () => {
   );
 };
 
-const DoctorInfo = memo(({
-  activeIndex,
-  // doctorInfo,
-  index,
-  person
-}: {
-  activeIndex: number | null;
-  // doctorInfo: IPerson | undefined;
-  index: number;
-  person: IPerson | undefined;
+const DoctorInfo = memo(
+  ({
+    activeIndex,
+    // doctorInfo,
+    index,
+    person,
+  }: {
+    activeIndex: number | null;
+    // doctorInfo: IPerson | undefined;
+    index: number;
+    person: IPerson | undefined;
+  }) => {
+    // console.log('person + doctorInfo', person, doctorInfo);
 
-}) => {
-  // console.log('person + doctorInfo', person, doctorInfo);
-  
-  return (
-    <div
-      className={`absolute inset-0 flex h-full flex-col gap-[6px] rounded-[20px] bg-indigo p-3 text-white transition-opacity ease-in-out ${
-        activeIndex === index
-      ? "opacity-100 duration-[700ms] delay-[400ms]"
-      : "opacity-0 duration-[700ms] delay-[0ms]"
-      }`}
-    >
-      {person && (
-        <>
-          <p className="geologica-text mt-auto text-[24px] font-medium leading-[24px] tracking-tighter">
-            {person.name}
-          </p>
-          <p className="text-[15px] font-normal lowercase leading-[18px] tracking-tighter">
-            {person.specialization}
-          </p>
-        </>
-      )}
-    </div>
-  );
-})
+    return (
+      <div
+        className={`absolute inset-0 flex h-full flex-col gap-[6px] rounded-[20px] bg-indigo p-3 text-white transition-opacity ease-in-out ${
+          activeIndex === index
+            ? "opacity-100 delay-[0] duration-[900ms]"
+            : "opacity-0 delay-[0] duration-[100ms]"
+        }`}
+      >
+        {person && (
+          <>
+            <p className="geologica-text mt-auto text-[24px] font-medium leading-[24px] tracking-tighter">
+              {person.name}
+            </p>
+            <p className="text-[15px] font-normal lowercase leading-[18px] tracking-tighter">
+              {person.specialization}
+            </p>
+          </>
+        )}
+      </div>
+    );
+  },
+);
