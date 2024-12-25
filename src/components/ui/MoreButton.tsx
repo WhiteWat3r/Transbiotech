643b075c869872549a4e96a0af8a7d806a6c4071
unknown <goirkopf@gmail.com>
2024-12-25 18:01:20 +0300

    правка кнопки

--- src/components/ui/MoreButton.tsx
+++ src/components/ui/MoreButton.tsx
@@ -1,7 +1,7 @@
 export const MoreButton = ({type = 'filled'} : {type?: 'filled' | 'ghost '}) => {
   return (
     <button className={`flex max-w-[93px] items-center justify-center rounded-[45px] ${type === 'filled' ? 'bg-indigo' : 'bg-white'}
-           mob-button transition-all duration-500 w-[97px] h-[27px]
+           mob-button transition-all duration-500 w-[97px] h-[27px] desktop:w-[141px]
           hover:bg-indigo hover:text-white desktop:h-[38px] desktop:max-w-[141px] desktop:bg-[white] desktop:text-[18px] 
           desktop:leading-[22px] desktop:text-black-default desktop:hover:translate-y-[-10px]`}>
       подробнее
